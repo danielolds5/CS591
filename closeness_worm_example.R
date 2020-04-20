5aa09014a2409f84b7a4bed668d094160a9d05a4
Coby Soss <coby.soss@wsu.edu>
2020-04-19 19:14:49 -0700

    closeness centrality code

---
+++ closeness_worm_example.R
@@ -0,0 +1,41 @@
+library(igraph); 
+g1 = read.graph("C:\\Users\\cobysoss\\Desktop\\networks\\celegansneural.gml", "gml")
+closenessCen = closeness(g1)
+nodes <- 1:297
+
+df = data.frame(nodes, closenessCen)
+df <- df[order(df$closenessCen),]
+numBlue = (nrow(df)/3) 
+j = 0
+for(i in 1:nrow(df)) 
+{
+   node = data.frame(df[i, 1])
+   if(j < numBlue)
+   {
+      V(g1)[unlist(node)]$color <- "blue"
+   }
+   else
+   {
+      V(g1)[unlist(node)]$color <- "red"
+   }
+   j <- j+1
+}
+
+summary(g1)
+print(g1,
+      graph.attributes = igraph_opt("print.graph.attributes"),
+      vertex.attributes = igraph_opt("print.vertex.attributes"),
+      edge.attributes = igraph_opt("print.edge.attributes"), names = TRUE,
+      max.lines = igraph_opt("auto.print.lines"))
+
+plot( g1, layout = layout_with_kk,
+      edge.width = 1,
+      edge.arrow.width = 0.3,
+      vertex.size = 3,
+      edge.arrow.size = 0.01,
+      vertex.size2 = 3,
+      vertex.label = NA,
+      asp = 1,
+      margin = 0)
+
+

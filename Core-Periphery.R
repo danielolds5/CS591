d593e40f76c5a9f8778065073a9460003749a9d7
danielolds5 <31669980+danielolds5@users.noreply.github.com>
2020-04-30 22:24:32 -0700

    Add files via upload

--- Core-Periphery.R
+++ Core-Periphery.R
@@ -1,15 +1,11 @@
 library(igraph)
+library(ggraph)
+library(tidyverse)
 
-par(mar = c(1, 1, 1, 1)) # Set the margin on all sides to 1
 NN = read.graph("celegansneural.gml", format = c("gml"))
+
 nodes <- 1: 297
 degree_NN <- degree(NN)
-length_degree = length(degree_NN)
-for(j in 1:length_degree)
-{
-  degree_NN[j] = .5 * degree_NN[j]  
-}
-
 df = data.frame(nodes, degree_NN)
 df <- df[order(df$degree_NN, decreasing = TRUE),]
 
@@ -30,15 +26,13 @@ for(i in 1:nrow(df))
     k_best = i
   }
 }
-core_nodes = c()
+
 for(i in 1:nrow(df)) 
 {
   node = data.frame(df[i, 1])
 
   if(i <= k_best)
   {
-    new_element = unlist(node)
-    core_nodes = c(core_nodes, new_element)
     V(NN)[unlist(node)]$color <- "red"
   }
   else
@@ -47,49 +41,145 @@ for(i in 1:nrow(df))
   }
 }
 
-plot( NN,layout = layout_with_kk,
+plot( NN, layout = layout_with_kk,
       edge.width = 1,
       edge.arrow.width = 0.3,
-      vertex.size = 6,
+      vertex.size = 3,
       edge.arrow.size = 0.01,
       vertex.size2 = 3,
       vertex.label = NA,
       asp = 1,
       margin = 0)
 
-###############begin page rank comparison####################
-core_and_pr_intersect = c()
-page_ranks = page.rank(NN)
+P <- function(i, U){  
+  matrix = as_adjacency_matrix()
+  for(j in U) 
+    P = sum(matrix[i,j]) + degree(i) / max(degree(NN))
+  return(P)
+}
+
+getNeighbors <- function(U){ 
+  totalNeighbors = c()
+  for(u in U)
+    u = unlist(u)
+    Neighbors = neighbors(NN, V(NN)[u], mode = "all")
+    totalNeighbors = union(Neighbors, totalNeighbors)
+    return(totalNeighbors)
+}
+
+reRank <- function(X) {
+  U <- c() 
+  V <- df$nodes
+  V1 <- V    
+  
+  for(i in 1:nrow(df)) 
+  {   
+    u = data.frame(df[i, 1])
+    V1 <- V1[-1]
+    break
+  }
+  U <- c(u)
+  while (length(V1) != 0)
+  {
+    for(i in getNeighbors(U)) 
+    {
+      P(i, U)
+      
+    }
+  }
+  
+
+} 
+
+
+
+############################################
+Z <- function(graph1)
+  
+  
+
+% 
+
+graph1 = read.graph("celegansneural.gml", format = c("gml"))
+
+degree_list <- list(degree(graph1, v = V(graph1), mode = "all"))
+
+degree_array <- array(as.numeric(unlist(degree_list)))
+
+
+% S is all the nodes in the graph 1-n 
+
+% Sort Degree in Descending order
+degree_sort <- sort(degree_array, decreasing = TRUE)
+
+degree_sort
+
+sum(degree_sort)
+
+n <- length(degree_sort)
+z <- 1:n
+X <- degree_sort  
+for(k in z){
+  for(i in 1:k){
+    X = (1/2)*sum(i)
+  }
+}
+
+
+Z <- graph_degree  
+    Z = 1/2*sum(graph_degree)
+
+Z_best = 297*297
+K_best = 0 
+
+for(i in 1:nrow(df)) 
+  {   
+    print(unlist(df[i,2]))
+    node = data.frame(df[i, 1])                                                                                                         
+    Z = Z + i - 1 - unlist(df[i,2]) 
+    if(Z < Z_best)
+    { 
+      Z_best = Z
+      k_best = i
+    }
+  } 
+
+
+g1 = read.graph("C:\\Users\\cobysoss\\Desktop\\networks\\celegansneural.gml", "gml")
+area = vcount(g1)^2
+page_ranks = page.rank(g1)
+nodes <- 1:297
 df = data.frame(nodes, page_ranks$vector)
-df <- df[order(df$page_ranks.vector,decreasing = TRUE),]
+df <- df[order(df$page_ranks.vector),]
+numBlue = (nrow(df)/3) 
 j = 0
 for(i in 1:nrow(df)) 
 {
   pageRank <- data.frame(df[i,2])
   node = data.frame(df[i, 1])
-  if(j < 18)
+  if(i < numBlue)
   {
-    if(node %in% core_nodes)
-    {
-      V(NN)[unlist(node)]$color <- "red"
-      core_and_pr_intersect = c(core_and_pr_intersect, unlist(node))
-    }
-    else
-    {
-      V(NN)[unlist(node)]$color <- "yellow"
-    }
+    V(g1)[unlist(node)]$color <- "blue"
   }
   else
   {
-    V(NN)[unlist(node)]$color <- "blue"
+    V(g1)[unlist(node)]$color <- "red"
   }
   j <- j+1
 }
 
-plot( NN, layout = layout_with_kk,
+
+summary(g1)
+print(g1,
+      graph.attributes = igraph_opt("print.graph.attributes"),
+      vertex.attributes = igraph_opt("print.vertex.attributes"),
+      edge.attributes = igraph_opt("print.edge.attributes"), names = TRUE,
+      max.lines = igraph_opt("auto.print.lines"))
+
+plot( g1, layout = layout_with_kk,
       edge.width = 1,
       edge.arrow.width = 0.3,
-      vertex.size = 6,
+      vertex.size = 3,
       edge.arrow.size = 0.01,
       vertex.size2 = 3,
       vertex.label = NA,
@@ -97,150 +187,20 @@ plot( NN, layout = layout_with_kk,
       margin = 0)
 
 
-############Closeness comparison#################
-closenessCen = closeness(NN)
-nodes <- 1:297
 
-df = data.frame(nodes, closenessCen)
-df <- df[order(df$closenessCen, decreasing = TRUE),]
-j = 0
+
+
 for(i in 1:nrow(df)) 
 {
+  pageRank <- data.frame(df[i,2])
   node = data.frame(df[i, 1])
-  if(j < 18)
+  if(i < numBlue)
   {
-    if(node %in% core_nodes)
-    {
-      V(NN)[unlist(node)]$color <- "red"  
-    }
-    else
-    {
-      V(NN)[unlist(node)]$color <- "yellow"
-    }
+    V(g1)[unlist(node)]$color <- "blue"
   }
   else
   {
-    V(NN)[unlist(node)]$color <- "blue"
+    V(g1)[unlist(node)]$color <- "red"
   }
   j <- j+1
 }
-
-plot( NN, layout = layout_with_kk,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      vertex.size = 6,
-      edge.arrow.size = 0.01,
-      vertex.size2 = 3,
-      vertex.label = NA,
-      asp = 1,
-      margin = 0)
-
-####################Betweeness centrality##################################
-between = betweenness(NN)
-nodes <- 1:297
-core_and_pr_and_betweenness = c()
-
-df = data.frame(nodes, between)
-df <- df[order(df$between, decreasing = TRUE),]
-j = 0
-for(i in 1:nrow(df)) 
-{
-  node = data.frame(df[i, 1])
-  if(j < 18)
-  {
-    if(node %in% core_nodes)
-    {
-      if(node %in% core_and_pr_intersect)
-      {
-        core_and_pr_and_betweenness = c(core_and_pr_and_betweenness, unlist(node))
-      }
-      V(NN)[unlist(node)]$color <- "red"  
-    }
-    else
-    {
-      V(NN)[unlist(node)]$color <- "yellow"
-    }
-  }
-  else
-  {
-    V(NN)[unlist(node)]$color <- "blue"
-  }
-  j <- j+1
-}
-
-plot( NN, 
-      layout = layout_with_kk,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      vertex.size = 6,
-      edge.arrow.size = 0.01,
-      vertex.size2 = 3,
-      vertex.label = NA,
-      asp = 1,
-      margin = 0)
-
-####################Eccentricity centrality##################################
-ecc = eccentricity(NN)
-nodes <- 1:297
-
-df = data.frame(nodes, ecc)
-df <- df[order(df$ecc, decreasing = TRUE),]
-j = 0
-for(i in 1:nrow(df)) 
-{
-  node = data.frame(df[i, 1])
-  if(j < 18)
-  {
-    if(node %in% core_nodes)
-    {
-      V(NN)[unlist(node)]$color <- "red"  
-    }
-    else
-    {
-      V(NN)[unlist(node)]$color <- "yellow"
-    }
-  }
-  else
-  {
-    V(NN)[unlist(node)]$color <- "blue"
-  }
-  j <- j+1
-}
-
-plot( NN, 
-      layout = layout_with_kk,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      vertex.size = 6,
-      edge.arrow.size = 0.01,
-      vertex.size2 = 3,
-      vertex.label = NA,
-      asp = 1,
-      margin = 0)
-
-
-##########Hyper central nodes####################################
-
-nodes = 1:297
-for(i in nodes)
-{
-  if(i %in% core_and_pr_and_betweenness)
-  {
-    V(NN)[i]$color <- "red"
-  }
-  else
-  {
-    V(NN)[i]$color <- "blue"
-  }
-}
-
-plot( NN, 
-      layout = layout_with_kk,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      vertex.size = 6,
-      edge.arrow.size = 0.01,
-      vertex.size2 = 3,
-      vertex.label = NA,
-      asp = 1,
-      margin = 0)
